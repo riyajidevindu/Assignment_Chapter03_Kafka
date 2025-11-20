dfaea5fa2195c1a49ccea6a6ab90960e71b50a15
riyajidevindu <riyajidevindu@gmail.com>
2025-11-20 20:49:47 +0530

    Add ProducerServiceApplication class for producer service initialization

--- producer-service/src/main/java/com/example/producer/ProducerServiceApplication.java
+++ producer-service/src/main/java/com/example/producer/ProducerServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.producer_service;
+package com.example.producer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ProducerServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ProducerServiceApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ProducerServiceApplication.class, args);
+    }
 }
